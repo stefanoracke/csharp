3c4d363f2ff33f00cb08e2c3a901619c78d94c87
stefanoracke <80729784+stefanoracke@users.noreply.github.com>
2022-07-25 21:47:43 -0300

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Write your name: ");
+string nombre = Console.ReadLine();
+
+
+
+//Edad
+
+Console.WriteLine("Write your age: ");
+string age = Console.ReadLine();
+
+int num_age = Int32.Parse(age);
+
+//City
+Console.WriteLine("Where you live?: ");
+string city = Console.ReadLine();
+
+
+Console.WriteLine("Hello " + nombre + ", your age is "+ age + " you live in "+ city);
